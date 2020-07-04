e58bab812484c7dddaf292bdca0ed0b4e3ab7986
Jonathan Jopio <jonathanjopio@gmail.com>
2020-07-04 21:17:16 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,5 +1,15 @@
 class Triangle
+  attr_accessor :length1, :length2, :length3
   
+  def initialize(length1, length2, length3)
+    @length1 = length1
+    @length2 = length2
+    @length3 = length3
+  end
+  
+  def kind
+    
+  end
   
   class TriangleError < StandardError
     
